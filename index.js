e978352f5a75010b6f693aff7a160abfa188d76f
Leilani Migisi <leilani.migisi@student.moringaschool.com>
2022-06-07 09:49:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function multiplyNumbers(num1,num2){
-    let multiplication =(num1*num2)
-    return multiplication
-  }
-   console.log (multiplyNumbers(31,2))
+const num1=31 ;
+const num2=2 ;
+const multiply = num1 * num2 ;
+const random = +1;
+const num3=29;
+const num4=5;
+const mod = num3 % num4 ;
+const max= Math.max(1,2,3,4,5,6,7,8,9,10,15,18,20);
